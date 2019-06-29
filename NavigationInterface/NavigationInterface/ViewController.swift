2727a7302d0286c4bed3d23e2b55f7d1fe32cdfd
nspangbo <nspangbo@gmail.com>
2019-06-29 23:30:46 +0800

    add NavigationInterface xcode project

---
+++ NavigationInterface/NavigationInterface/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NavigationInterface
+//
+//  Created by nspangbo on 2019/6/29.
+//  Copyright © 2019 nspangbo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
